64f1eccc458872555fc79a9d8f2b51b6dafb712c
Markus Schwarz <post@markus-schwarz.net>
2019-07-21 20:26:55 +0200

    Always start reading topic from the beginning.

--- src/test/java/com/github/marschwar/kafkasampler/KafkaMessageSamplerIntegrationTest.java
+++ src/test/java/com/github/marschwar/kafkasampler/KafkaMessageSamplerIntegrationTest.java
@@ -105,6 +105,7 @@ class KafkaMessageSamplerIntegrationTest {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         Consumer<String, String> consumer = new KafkaConsumer<>(props);
 
         kafkaEmbedded.consumeFromAnEmbeddedTopic(consumer, topic);
